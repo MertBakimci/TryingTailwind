7a70e02ee9d0c6cf0f398f09a3038abc96f76bb8
MertBakimci <bkmmert2004@gmail.com>
2022-01-15 17:37:37 +0300

    daisyui form and navbar

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -5,7 +5,7 @@ const Form = () => {
     <div className="mt-0 pt-4 pl-4 h-[100%] bg-violet-700 bg-gradient-to-r from-violet-700 to-violet-900 mx-auto w-full">
       <div className="w-[100%] max-w-[1920px] relative h-full sm:w-[95%] lg:w-[85%] flex items-center  mx-auto">
         <img className="w-[70%] max-w-[700px] md:top-50 right-20 md:right-10 absolute bottom-0 mt-auto" src={banner} alt="" />
-        <div class="form-control absolute left-0 md:top-1/2 md:transform  md:-translate-y-1/2  z-20 w-full md:mb-5 md:ml-5 max-w-[600px] bg-violet-700 p-5  md:rounded-xl md:shadow-lg">
+        <div class="form-control absolute top-20 text-black left-0 md:top-1/2 md:transform  md:-translate-y-1/2  z-20 w-full md:mb-5 md:ml-5 max-w-[600px] bg-violet-700 p-5  md:rounded-xl md:shadow-lg">
           <div className="form-control flex flex-col md:flex-row md:gap-5">
             <div className="w-full md:w1/2">
               <label class="label">
