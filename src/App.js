fc74fdfcbb32369509f7d7dde0a8d3bc072a11da
MertBakimci <bkmmert2004@gmail.com>
2022-01-17 11:35:36 +0300

    deleted my own stupidity

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
    <div className="container h-screen bg-gray-100 w-full sm:w-[100%} max-w-none">
     <Navbar/>
       <Form/>
-  
-
+     
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,11 +3,7 @@ import React, {useState,useEffect} from "react";
 const Navbar = () => {
     const [menu,setMenu] = useState(false);
 
-    useEffect(() => {
-       if (window.innerWidth > 768) {
-        setMenu(true)
-       }
-    }, [])
+ 
     useEffect(() => {
      if(menu === true) {
        document.getElementById('nav').classList.toggle("left-0");
@@ -19,7 +15,7 @@ const Navbar = () => {
     }, [menu])
   return (
     <div>
-      <div className={`overlay z-40 transition-all duration-500 ease-in-out absolute top-0 left-0 h-[100vh] w-[100vw] bg-black ${menu ? "visible opacity-50" : "invisible opacity-0"}`}></div>
+      <div className={`overlay z-40 transition-all visible md:invisible duration-500 ease-in-out absolute top-0 left-0 h-[100vh] w-[100vw] bg-black ${menu ? "visible opacity-50" : "invisible opacity-0 "}`}></div>
     <nav className="navbar max-w-none fixed z-50 top-0 bg-gradient-to-r from-violet-700 to-violet-900 border-b-2 border-opacity-20 border-gray-100 w-full shadow-lg bg-violet-600 text-neutral-content ">
       <div className="container  max-w-none sm:w-[95%] lg:w-[85%] flex flex-wrap mx-auto">
           <span class="text-xl font-bold">Navbar</span>
@@ -39,7 +35,7 @@ const Navbar = () => {
               </svg>
             </button>
           </div>
-          <div id="nav" class={`transition-all ease-in-out duration-500 absolute z-50 md:h-auto md:w-auto md:sticky top-[96%] h-[100vh]  bg-gradient-to-l md:bg-gradient-to-r md:from-transparent md:to-transparent  from-violet-800  to-violet-700  p-1  md:ml-auto justify-start items-center flex flex-col  md:flex-row w-[60%] md:order-1`}>
+          <div id="nav" class={`transition-all  ease-in-out duration-500 absolute z-50 md:h-auto md:w-auto md:sticky top-[96%] h-[100vh]  bg-gradient-to-l md:bg-gradient-to-r md:from-transparent md:to-transparent  from-violet-800  to-violet-700  p-1  md:ml-auto justify-start items-center flex flex-col  md:flex-row w-[60%] md:order-1`}>
             <a href="#" class="w-full mt-4 md:mt-0 md:w-auto btn btn-ghost btn rounded-btn">Home</a>
               <a href="#" class="w-full md:w-auto btn btn-ghost btn rounded-btn">daısyUI</a>
               <a href="#" class="w-full md:w-auto btn btn-ghost btn rounded-btn">Tailwind</a>
